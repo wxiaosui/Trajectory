7fa8770c675b468c0c5a84e70082020c650868da
wxiaosui <wangxsui@gmail.com>
2021-05-11 00:22:23 +0800

    接口与分层

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { resolve } = require('path');
 const { promisify } = require('util');
+const initControllers = require('./controllers');
 
 const server = express();
 const port = parseInt(process.env.PORT || '9000');
@@ -8,6 +9,7 @@ const publicDir = resolve('public');
 
 async function bootstrap() {
     server.use(express.static(publicDir));
+    server.use(await initControllers());
     await promisify(server.listen.bind(server, port))();
     console.log(`> Started on port ${port}`);
 }
